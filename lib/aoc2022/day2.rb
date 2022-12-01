c9262cb925f2ab31ce0ba016f5294e15da3ce3a3
Tobias Eilert <github@pentokami.de>
2022-12-01 18:44:22 +0100

    chore: prepare day 2

---
+++ lib/aoc2022/day2.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Aoc2022
+  module Day2
+    module_function
+
+    def solve_example(input)
+      solve_part1(input)
+    end
+
+    def solve_part1(input); end
+
+    def solve_part2(input); end
+  end
+end
---
+++ spec/day2_spec.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+RSpec.describe Aoc2022::Day2 do
+end
