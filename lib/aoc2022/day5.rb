31dee80fd1b7f7919b20202fd4a29b972c590f5d
Tobias Eilert <github@pentokami.de>
2022-12-07 20:26:24 +0100

    feat: day 5 part 1

---
+++ lib/aoc2022/day5.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module Aoc2022
+  module Day5
+    Instruction = Struct.new(:from, :to, :amount)
+
+    module_function
+
+    def parse_crates(str)
+      lines = str.split("\n").filter_map do |line|
+        if line =~ / 1 /
+          nil
+        else
+          group_matches = line.scan(/\s{4}|([A-Z])/)
+          group_matches.map(&:compact)
+        end
+      end
+
+      crates = []
+      lines.reverse.each do |l|
+        l.each_with_index do |c, i|
+          crates[i] ||= []
+
+          crates[i] << c[0]
+        end
+      end
+      crates.map(&:compact)
+    end
+
+    def parse_instruction(instr)
+      found = instr.match(/move (\d+) from (\d+) to (\d+)/).captures.map(&:to_i)
+      Instruction.new(found[1], found[2], found[0])
+    end
+
+    def parse_instructions(instr)
+      instr.split("\n").map { |i| parse_instruction(i) }
+    end
+
+    def parse_input(str)
+      s = str.split("\n")
+      i = s.index("")
+
+      crates = parse_crates(s[0..(i - 1)].join("\n"))
+      instr = parse_instructions(s[(i + 1)..-1].join("\n"))
+
+      [crates, instr]
+    end
+
+    def apply_instruction!(crates, instruction)
+      instruction.amount.times do
+        c = crates[instruction.from - 1].pop
+        crates[instruction.to - 1].push(c)
+      end
+
+      crates
+    end
+
+    def top_crates(crates)
+      crates.map(&:last).join("")
+    end
+
+    def solve_example(input)
+      solve_part1(input)
+    end
+
+    def solve_part1(input)
+      crates, instr = parse_input(input)
+
+      instr.each do |i|
+        crates = apply_instruction!(crates, i)
+      end
+
+      top_crates(crates)
+    end
+
+    def solve_part2(input); end
+  end
+end
---
+++ spec/day5_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+Day5 = Aoc2022::Day5
+Instruction = Day5::Instruction
+
+RSpec.describe Aoc2022::Day5 do
+  context "crates" do
+    it "parses crates" do
+      crates = Day5.parse_crates("    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 ")
+
+      expect(crates).to eq([%w[Z N], %w[M C D], ["P"]])
+    end
+  end
+
+  context "instructions" do
+    it "parses one instruction" do
+      inst = Day5.parse_instruction("move 1 from 2 to 1")
+
+      expect(inst).to eq(Instruction.new(2, 1, 1))
+    end
+
+    it "parses multiple instructions" do
+      inst = Day5.parse_instructions("move 1 from 2 to 1\nmove 1 from 2 to 3\nmove 4 from 10 to 8")
+
+      expect(inst.size).to eq(3)
+      expect(inst[0]).to eq(Instruction.new(2, 1, 1))
+      expect(inst[1]).to eq(Instruction.new(2, 3, 1))
+      expect(inst[2]).to eq(Instruction.new(10, 8, 4))
+    end
+  end
+
+  context "crate stacking" do
+    it "can apply an instruction on crates" do
+      crates = [%w[Z N], %w[M C D], ["P"]]
+      instruction = Instruction.new(2, 1, 1)
+
+      new_stack = Day5.apply_instruction!(crates, instruction)
+      expect(new_stack).to eq([%w[Z N D], %w[M C], ["P"]])
+
+      instruction = Instruction.new(3, 2, 1)
+      new_stack = Day5.apply_instruction!(crates, instruction)
+      expect(new_stack).to eq([%w[Z N D], %w[M C P], []])
+    end
+
+    it "can move multiple crates one after another" do
+      crates = [%w[Z N], %w[M C D], ["P"]]
+      instruction = Instruction.new(2, 3, 2)
+
+      new_stack = Day5.apply_instruction!(crates, instruction)
+      expect(new_stack).to eq([%w[Z N], %w[M], %w[P D C]])
+    end
+  end
+
+  context "top crates" do
+    it "can show all top row crates as result string" do
+      crates = [%w[Z N], %w[M C D], ["P"]]
+      top = Day5.top_crates(crates)
+
+      expect(top).to eq("NDP")
+    end
+  end
+end
