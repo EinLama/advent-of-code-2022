c551b93dd1160a430f9a0fb3251527386d619cf7
Tobias Eilert <github@pentokami.de>
2022-12-23 17:10:35 +0100

    feat: load each day

--- lib/aoc2022.rb
+++ lib/aoc2022.rb
@@ -2,15 +2,7 @@
 
 require "optparse"
 
-require_relative "aoc2022/version"
-require_relative "aoc2022/day1"
-require_relative "aoc2022/day2"
-require_relative "aoc2022/day3"
-require_relative "aoc2022/day4"
-require_relative "aoc2022/day5"
-require_relative "aoc2022/day6"
-require_relative "aoc2022/day7"
-require_relative "aoc2022/day8"
+Dir[File.join(__dir__, "aoc2022", "*.rb")].sort.each { |file| require file }
 
 module Aoc2022
   class Error < StandardError; end
